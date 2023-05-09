5a71d24259505ac05a91ad9b58505a2c464bb8b1
Daniel Oliveira <gbdaniel_rj@hotmail.com>
2023-05-08 23:25:52 -0300

    subindo url  documentação

--- service-discovery/src/main/java/br/com/dio/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/br/com/dio/ServiceDiscoveryApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//https://cloud.spring.io/spring-cloud-netflix/reference/html/
 @EnableEurekaServer
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
